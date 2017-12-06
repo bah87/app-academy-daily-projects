107f2593aebf3ba2113d309332e55c9a9975471b
Brendan Higgins <bhiggins033@gmail.com>
2017-12-06 18:06:08 -0500

    Final commit rails lite W5D3

--- W5D3/lib/controller_base.rb
+++ W5D3/lib/controller_base.rb
@@ -2,39 +2,67 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'active_support/inflector'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
+    @req = req
+    @res = res
+    @params = route_params.merge(req.params)
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if already_built_response?
+      raise "Double render error"
+    else
+      self.res.status = 302
+      self.res['Location'] = url
+      self.session.store_session(self.res)
+      @already_built_response = true
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    if already_built_response?
+      raise "Double render error"
+    else
+      self.res['Content-Type'] = content_type
+      self.res.write(content)
+      self.session.store_session(self.res)
+      @already_built_response = true
+    end
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.to_s.underscore
+    template_contents = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    rendered_html = ERB.new(template_contents).result(binding)
+    render_content(rendered_html, "text/html")
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
 
